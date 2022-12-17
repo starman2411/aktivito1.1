07a2b1d6510d2c3373dc9f6941e904c1f46601ab
Nikita_Bakery <nikitaf71@gmail.com>
2022-12-18 01:19:42 +0300

    Обновление первого обработчика, окончательное добавление цветов и транскрипция русских названий

--- aktivito_handler/templatetags/valid_name.py
+++ aktivito_handler/templatetags/valid_name.py
@@ -4,8 +4,11 @@ from transliterate import translit
 
 @register.filter
 def make_valid_name(ru_text):
+    ru_text = ' '.join(ru_text.split())
     en_text = translit(ru_text, language_code='ru', reversed=True)
-    text_list = list(map(lambda s: s[0].upper() + s[1:], en_text.split(' ')))
-    return ''.join(text_list)
+    if len(en_text)>0:
+        text_list = list(map(lambda s: s[0].upper() + s[1:], en_text.split(' ')))
+        return ''.join(text_list)
+    return '_'
 
 
--- aktivito_handler/views.py
+++ aktivito_handler/views.py
@@ -23,7 +23,7 @@ from transliterate import translit
 def make_valid_name(ru_text):
     ru_text = ' '.join(ru_text.split())
     en_text = translit(ru_text, language_code='ru', reversed=True)
-    if len(en_text)>1:
+    if len(en_text)>0:
         text_list = list(map(lambda s: s[0].upper() + s[1:], en_text.split(' ')))
         return ''.join(text_list)
     return '_'
