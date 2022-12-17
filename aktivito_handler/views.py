9bce02afa4bee0ad5fd558aae6095c191265ecda
Nikita_Bakery <nikitaf71@gmail.com>
2022-12-18 01:00:19 +0300

    Обновление первого обработчика, окончательное добавление цветов и транскрипция русских названий

--- aktivito_handler/views.py
+++ aktivito_handler/views.py
@@ -22,8 +22,10 @@ from transliterate import translit
 
 def make_valid_name(ru_text):
     en_text = translit(ru_text, language_code='ru', reversed=True)
-    text_list = list(map(lambda s: s[0].upper() + s[1:], en_text.split(' ')))
-    return ''.join(text_list)
+    if len(en_text)>1:
+        text_list = list(map(lambda s: s[0].upper() + s[1:], en_text.split(' ')))
+        return ''.join(text_list)
+    return '_'
 
 
 def get_preset(preset_name):
